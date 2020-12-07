5773612a96c6f534bdca4eea218702a2e819cfc6
MrSkillage <43910132+MrSkillage@users.noreply.github.com>
2020-12-07 22:56:05 +0000

    Implemented banking-service and user-notification-service and working.

--- bank-api-service/src/main/java/Application.java
+++ bank-api-service/src/main/java/Application.java
@@ -1,4 +1,5 @@
 import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.LongSerializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
@@ -8,19 +9,32 @@ import java.util.concurrent.ExecutionException;
  * Banking API Service
  */
 public class Application {
+    private static final String VALID_TOPIC = "valid-transactions";
+    private static final String SUSPICIOUS_TOPIC = "suspicious-transactions";
+    private static final String BOOTSTRAP_SERVERS = "localhost:9092,localhost:9093,localhost:9094";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         IncomingTransactionsReader incomingTransactionsReader = new IncomingTransactionsReader();
         CustomerAddressDatabase customerAddressDatabase = new CustomerAddressDatabase();
 
-        //*****************
-        // YOUR CODE HERE
-        //*****************
+        Application kafkaApp = new Application();
+        Producer<String, Transaction> kafkaProducer = kafkaApp.createKafkaProducer(BOOTSTRAP_SERVERS);
+
+        try {
+            processTransactions(incomingTransactionsReader, customerAddressDatabase, kafkaProducer);
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            kafkaProducer.flush();
+            kafkaProducer.close();
+        }
+
+
     }
 
-    public void processTransactions(IncomingTransactionsReader incomingTransactionsReader,
-                                    CustomerAddressDatabase customerAddressDatabase,
-                                    Producer<String, Transaction> kafkaProducer) throws ExecutionException, InterruptedException {
+    public static void processTransactions(IncomingTransactionsReader incomingTransactionsReader,
+                                           CustomerAddressDatabase customerAddressDatabase,
+            Producer<String, Transaction> kafkaProducer) throws ExecutionException, InterruptedException {
 
 
         // Retrieve the next transaction from the IncomingTransactionsReader
@@ -30,15 +44,49 @@ public class Application {
         // location match or not.
         // Print record metadata information
 
-        //*****************
-        // YOUR CODE HERE
-        //*****************
+        while (incomingTransactionsReader.hasNext()) {
+
+            Transaction info = incomingTransactionsReader.next();
+            String user = info.getUser();
+            double value = info.getAmount();
+
+            if (info.getTransactionLocation().equals(customerAddressDatabase.getUserResidence(user))) {
+
+                ProducerRecord<String, Transaction> record = new ProducerRecord<>(VALID_TOPIC, user, info);
+
+                RecordMetadata recordMetadata = kafkaProducer.send(record).get();
+
+                System.out.println(String.format("[User: %s, Amount: %f, Loc: %s, Home: %s, MATCHED!",
+                        user, value, info.getTransactionLocation(), customerAddressDatabase.getUserResidence(user)));
+
+
+            } else {
+
+                ProducerRecord<String, Transaction> record = new ProducerRecord<>(SUSPICIOUS_TOPIC, user, info);
+
+                RecordMetadata recordMetadata = kafkaProducer.send(record).get();
+
+                System.out.println(String.format("[User: %s, Amount: %f, Loc: %s, Home: %s, SUSPICIOUS!",
+                        user, value, info.getTransactionLocation(), customerAddressDatabase.getUserResidence(user)));
+            }
+
+        }
+
+
     }
 
-//    public Producer<String, Transaction> createKafkaProducer(String bootstrapServers) {
-    //*****************
-    // YOUR CODE HERE
-    //*****************
-//    }
+    public Producer<String, Transaction> createKafkaProducer(String bootstrapServers) {
+        // Make a new Properties object called prop
+        Properties prop = new Properties();
+
+        // Set the various Properties used to setup a Producer Configuration
+        prop.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        prop.put(ProducerConfig.CLIENT_ID_CONFIG, "banking-api");
+        prop.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        prop.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, Transaction.TransactionSerializer.class.getName());
+
+        // Returns a new KafkaConsumer made with the properties we set in prop
+        return new KafkaProducer<String, Transaction>(prop);
+    }
 
 }
--- user-notification-service/src/main/java/Application.java
+++ user-notification-service/src/main/java/Application.java
@@ -1,37 +1,76 @@
 import org.apache.kafka.clients.consumer.*;
 import org.apache.kafka.common.serialization.LongDeserializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 
+import javax.swing.*;
 import java.time.Duration;
 import java.util.Collections;
 import java.util.Properties;
 
 public class Application {
 
+    // Final Strings of the Topic and Servers available
+    private static final String TOPIC = "suspicious-transactions";
+    private static final String BOOTSTRAP_SERVERS = "localhost:9092.localhost:9093,localhost:9094";
+
     public static void main(String[] args) {
-        //*****************
-        // YOUR CODE HERE
-        //*****************
+
+        Application kafkaUserNotificationConsumerApp = new Application();
+
+        String consumerGroup = "user-notification-service";
+        // Print out message of which Consumer Group we belong to
+        System.out.println("Consumer is part of consumer group " + consumerGroup);
+
+        // Call createKafkaConsumer method and pass the Servers and Consumer Group
+        Consumer<String, Transaction> userConsumer = kafkaUserNotificationConsumerApp.createKafkaConsumer(BOOTSTRAP_SERVERS, consumerGroup);
+        // Call consumerMessages method and pass the TOPIC and Consumer we creates above
+        kafkaUserNotificationConsumerApp.consumeMessages(TOPIC, userConsumer);
+
     }
 
     public static void consumeMessages(String topic, Consumer<String, Transaction> kafkaConsumer) {
-        //*****************
-        // YOUR CODE HERE
-        //*****************
+        // Subscribe the consumer to the topic passed
+        kafkaConsumer.subscribe(Collections.singletonList(topic));
+
+        // Create and Infinite loop while continuously checking for new messages
+        while (true) {
+            // ConsumerRecords stores a list of ConsumerRecords and polls every 1 second
+            ConsumerRecords<String, Transaction> consumerRecords = kafkaConsumer.poll(Duration.ofSeconds(1));
+            // If consumerRecords is not empty i.e. has a record then do
+            if (!consumerRecords.isEmpty()) {
+                // For each ConsumerRecord in the list ConsumerRecords print a consume message with formatted details
+                for (ConsumerRecord<String, Transaction> record : consumerRecords) {
+                    System.out.println(String.format("Received record with (key: %s, value: %s)",
+                            record.key(), record.value().toString()));
+                }
+            } else {
+                // DO NOTHING FOR NOW
+            }
+            // Tell kafka its done processing messages with a commit as a final confirmation
+            kafkaConsumer.commitAsync();
+        }
+
     }
 
-//    public static Consumer<String, Transaction> createKafkaConsumer(String bootstrapServers, String consumerGroup) {
-    //*****************
-    // YOUR CODE HERE
-    //*****************
-//    }
+    public static Consumer<String, Transaction> createKafkaConsumer(String bootstrapServers, String consumerGroup) {
+        // Make a new Properties object called prop
+        Properties prop = new Properties();
+
+        // Set the various Properties used to setup a Consumer Configuration
+        prop.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        prop.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        prop.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, Transaction.TransactionDeserializer.class.getName());
+        prop.put(ConsumerConfig.GROUP_ID_CONFIG, consumerGroup);
+        prop.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
+
+        // Returns a new KafkaConsumer made with the properties we set in prop
+        return new KafkaConsumer<String, Transaction>(prop);
+    }
 
     private static void sendUserNotification(Transaction transaction) {
         // Print transaction information to the console
-
-        //*****************
-        // YOUR CODE HERE
-        //*****************
+        System.out.println(transaction.toString());
     }
 
 }
